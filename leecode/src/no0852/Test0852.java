aca8793fe59bd440debc6c599d2f6733fa9fc2ca
xiao <603426335@qq.com>
2019-10-14 19:50:54 +0800

    山脉数组的峰顶索引

---
+++ leecode/src/no0852/Test0852.java
@@ -0,0 +1,12 @@
+package no0852;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0852 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		int[] A = new int[] {3,4,5,1};
+		System.out.println(so.peakIndexInMountainArray(A));
+	}
+}
