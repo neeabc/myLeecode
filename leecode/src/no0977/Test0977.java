3ff65a2386e0f7496276739f54f97f7e03dfa190
hlt <11@qq.com>
2019-10-14 09:56:37 +0800

    平方数组排序

---
+++ leecode/src/no0977/Test0977.java
@@ -0,0 +1,15 @@
+package no0977;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0977 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		int[] A = new int[] {-3,-3,-2,1};
+		for(int a:so.sortedSquares(A)) {
+			System.out.println(a);
+		}
+		
+	}
+}
