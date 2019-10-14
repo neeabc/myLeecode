fc0252d72e36b79c9478c2ad5f50f78ad121f595
xiao <603426335@qq.com>
2019-10-14 20:05:37 +0800

    反转字符串中的单词 III

---
+++ leecode/src/no0557/Test0557.java
@@ -0,0 +1,12 @@
+package no0557;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0557 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		String s = new String("Let's take LeetCode contest");
+		System.out.println(so.reverseWords(s));
+	}
+}
