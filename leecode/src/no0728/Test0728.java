f4c8e8316042f704317662d717eb5986e19174b1
xiao <603426335@qq.com>
2019-10-14 12:58:36 +0800

    自除数

---
+++ leecode/src/no0728/Test0728.java
@@ -0,0 +1,13 @@
+package no0728;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class Test0728 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		so.selfDividingNumbers(1, 22);
+	}
+}
