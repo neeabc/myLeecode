3375b55c44dd000a5287d01e62f3867cb86bf3bf
xiao <603426335@qq.com>
2019-10-15 14:21:03 +0800

    删列造序

---
+++ leecode/src/no0944/Test0944.java
@@ -0,0 +1,15 @@
+package no0944;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0944 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		String[] A = new String[] {"zyx", "wvu", "tsr"};
+		
+		System.out.println(so.minDeletionSize(A));
+		
+	}
+}
