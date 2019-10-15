edc4fc824a0a50460f7863871c722a20b2d01cc5
xiao <603426335@qq.com>
2019-10-15 13:41:25 +0800

    键盘行

---
+++ leecode/src/no0500/Test0500.java
@@ -0,0 +1,15 @@
+package no0500;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0500 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		String[] words = new String[] {"Hello", "Alaska", "Dad", "Peace"};
+		
+		for(String word:so.findWords(words)) {
+			System.out.println(word);
+		}
+	}
+}
