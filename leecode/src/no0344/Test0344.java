b732155e87b774da7f955e2a48352a16217bdd47
xiao <603426335@qq.com>
2019-10-15 09:54:34 +0800

    反转字符串

---
+++ leecode/src/no0344/Test0344.java
@@ -0,0 +1,19 @@
+package no0344;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0344 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		char[] s = new char[] {' ','a',' ',','};
+		
+		so.reverseString(s);
+		
+		for(char m:s) {
+			System.out.print("|"+m+"|");
+		}
+		
+	}
+}
