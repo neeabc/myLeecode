79421dc61da4edabc9d7b6c69c482a491b7f3a2e
xiao <603426335@qq.com>
2019-10-18 09:59:03 +0800

    保持城市天际线

---
+++ leecode/src/no0807/Test0807.java
@@ -0,0 +1,14 @@
+package no0807;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0807 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[][] grid = new int[][] {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+		
+		int ans = so.maxIncreaseKeepingSkyline(grid);
+	}
+}
