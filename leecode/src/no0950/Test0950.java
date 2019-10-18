c3c60fc59aedd492effe9040696420cd38e6d3e3
xiao <603426335@qq.com>
2019-10-18 14:22:48 +0800

    按递增顺序显示卡牌

---
+++ leecode/src/no0950/Test0950.java
@@ -0,0 +1,15 @@
+package no0950;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0950 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[] deck = new int[] {17,13,11,2,3,5,7};
+		
+		so.deckRevealedIncreasing(deck);
+	}
+	
+}
