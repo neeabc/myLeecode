5ab848741984c7913f268e3b27f51dc2e19ed773
xiao <603426335@qq.com>
2019-10-19 10:51:44 +0800

    二叉搜索树的范围和

--- leecode/src/no0807/Test0807.java
+++ leecode/src/no0807/Test0807.java
@@ -9,6 +9,6 @@ public class Test0807 {
 		
 		int[][] grid = new int[][] {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
 		
-		int ans = so.maxIncreaseKeepingSkyline(grid);
+		so.maxIncreaseKeepingSkyline(grid);
 	}
 }
