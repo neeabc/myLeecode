9512210fd3a057baaab3726ae413e668c38f48e9
xiao <603426335@qq.com>
2019-10-22 11:00:11 +0800

    棒球比赛

---
+++ leecode/src/no0682/Test0682.java
@@ -0,0 +1,11 @@
+package no0682;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0682 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		so.calPoints(new String[]{"5","-2","4","C","D","9","+","+"});
+	}
+}
