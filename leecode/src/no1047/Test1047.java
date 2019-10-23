c53a11f113f86b8da8841e4834f48c5124a1223c
xiao <603426335@qq.com>
2019-10-23 20:09:35 +0800

    删除字符串中的所有相邻重复项

---
+++ leecode/src/no1047/Test1047.java
@@ -0,0 +1,11 @@
+package no1047;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1047 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		so.removeDuplicates("abbaca");
+	}
+}
