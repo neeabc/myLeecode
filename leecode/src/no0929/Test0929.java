42d01b83076fb7464bfb6fc41b74a142217737cc
xiao <603426335@qq.com>
2019-10-26 13:43:38 +0800

    独特的电子邮件地址

---
+++ leecode/src/no0929/Test0929.java
@@ -0,0 +1,12 @@
+package no0929;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0929 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+		so.numUniqueEmails(emails);
+	}
+}
