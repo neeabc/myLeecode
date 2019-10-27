6a333f585e7333360deba168f2a7553e70793384
xiao <603426335@qq.com>
2019-10-27 11:05:09 +0800

    子域名访问计数

---
+++ leecode/src/no0881/Test0881.java
@@ -0,0 +1,14 @@
+package no0881;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0881 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		String[] cpdomains = new String[] {"9001 discuss.leetcode.com"};
+		
+		so.subdomainVisits(cpdomains);
+	}
+}
