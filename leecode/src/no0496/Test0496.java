2e9621344e6598abceb7e2acff6601fd676adb69
xiao <603426335@qq.com>
2019-10-27 15:25:34 +0800

    下一个更大元素 I

---
+++ leecode/src/no0496/Test0496.java
@@ -0,0 +1,15 @@
+package no0496;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0496 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[] nums1 = {4,1,2};
+		int[] nums2 = {1,3,4,2};
+		
+		so.nextGreaterElement(nums1, nums2);
+	}
+}
