06205a0f7bc06af2b7eb7d052aacb63931183665
xiao <603426335@qq.com>
2019-10-30 20:46:36 +0800

    区域和检索 - 数组不可变

---
+++ leecode/src/no0303/Test0303.java
@@ -0,0 +1,11 @@
+package no0303;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0303 {
+	@Test
+	public void startTest() {
+		NumArray a = new NumArray(new int[] {-2,0,3,-5,2,-1});
+		a.sumRange(0, 2);
+	}
+}
