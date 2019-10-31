86e182d9b9d99d2ea491f498c0c525bd4967d160
xiao <603426335@qq.com>
2019-10-31 18:57:22 +0800

    复写零

---
+++ leecode/src/no1089/Test1089.java
@@ -0,0 +1,12 @@
+package no1089;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1089 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		int[] arr = {1,0,2,3,0,4,5,0};
+		so.duplicateZeros(arr);
+	}
+}
