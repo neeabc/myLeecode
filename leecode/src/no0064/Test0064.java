cdbde2c632aeb340af15bac56c9b5b9dc976d6e0
xiao <603426335@qq.com>
2019-11-01 20:36:10 +0800

    最小路径和

---
+++ leecode/src/no0064/Test0064.java
@@ -0,0 +1,12 @@
+package no0064;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0064 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
+		so.minPathSum(grid);
+	}
+}
