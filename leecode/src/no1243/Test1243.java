09511a0efe7b43ed85ee839521858c41bb27062a
xiao <603426335@qq.com>
2019-11-04 21:31:50 +0800

    数组变换

---
+++ leecode/src/no1243/Test1243.java
@@ -0,0 +1,14 @@
+package no1243;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1243 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[] arr = {6,2,3,4};
+		
+		so.transformArray(arr);
+	}
+}
