b99eaa46174e123c066137b4610706dcd520b57e
xiao <603426335@qq.com>
2019-11-06 21:12:35 +0800

    合并两个有序数组

---
+++ leecode/src/no0088/Test0088.java
@@ -0,0 +1,11 @@
+package no0088;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0088 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		so.merge(new int[] {0}, 0,new int[]{1},1);
+	}
+}
