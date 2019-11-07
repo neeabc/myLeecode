989245074047a13d5bb07a9de55284b7432eb752
xiao <603426335@qq.com>
2019-11-07 19:53:14 +0800

    两数之和 II - 输入有序数组

---
+++ leecode/src/no0167/Test0167.java
@@ -0,0 +1,11 @@
+package no0167;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0167 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		so.twoSum(new int[] {2, 7, 11, 15}, 9);
+	}
+}
