b8ea966f626c89782e3eb07198ef7292d1ae7c23
xiao <603426335@qq.com>
2019-11-09 20:22:40 +0800

    卡牌分组

---
+++ leecode/src/no0914/Test0914.java
@@ -0,0 +1,14 @@
+package no0914;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0914 {
+	@Test
+	public void startTest() {
+		Solution solution = new Solution();
+		
+		int[] deck = new int[] {1};
+		
+		solution.hasGroupsSizeX(deck);
+	}
+}
