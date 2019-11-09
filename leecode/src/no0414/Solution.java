27abc2d56bfb714555e60a3c70bb8c053b7b45ed
xiao <603426335@qq.com>
2019-11-09 17:03:05 +0800

    第三大的数

---
+++ leecode/src/no0414/Solution.java
@@ -0,0 +1,60 @@
+package no0414;
+/**
+给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+
+示例 1:
+
+输入: [3, 2, 1]
+
+输出: 1
+
+解释: 第三大的数是 1.
+示例 2:
+
+输入: [1, 2]
+
+输出: 2
+
+解释: 第三大的数不存在, 所以返回最大的数 2 .
+示例 3:
+
+输入: [2, 2, 3, 1]
+
+输出: 1
+
+解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+存在两个值为2的数，它们都排第二。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/third-maximum-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Administrator
+ *
+ */
+public class Solution {
+    public int thirdMax(int[] nums) {
+    	long[] base = new long[] {Long.MIN_VALUE,Long.MIN_VALUE,Long.MIN_VALUE,0};
+    	
+    	int temp;
+    	int j;
+        for(int i=0;i<nums.length;i++) {
+        	temp = nums[i];
+        	
+        	for(j=0;j<3;j++) {
+        		if(temp==base[j]) {
+        			j=3;
+        			break;
+        		}else if(temp>base[j]){
+        			break;
+        		}
+        	}
+        	
+        	for(int k=2;k>=j;k--) {
+        		base[k+1] = base[k];
+        	}
+        	base[j]=temp;
+        }
+        
+        return base[2]==Long.MIN_VALUE?(int)base[0]:(int)base[2];
+    }
+}
---
+++ leecode/src/no0414/Test0414.java
@@ -0,0 +1,14 @@
+package no0414;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0414 {
+	@Test
+	public void startTest() {
+		Solution solution = new Solution();
+		
+		int[] nums = new int[] {3,2,1};
+		
+		solution.thirdMax(nums);
+	}
+}
