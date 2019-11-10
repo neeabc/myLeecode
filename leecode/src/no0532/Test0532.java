1f95a39f19943372f4c6596093d34c905789dbfc
xiao <603426335@qq.com>
2019-11-10 15:19:06 +0800

    数组中的K-diff数对

---
+++ leecode/src/no0532/Test0532.java
@@ -0,0 +1,15 @@
+package no0532;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0532 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[] nums = new int[] {6,3,5,7,2,3,3,8,2,4};
+		int k = 2;
+		
+		so.findPairs(nums, k);
+	}
+}
