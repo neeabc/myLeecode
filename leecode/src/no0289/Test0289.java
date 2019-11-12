7d44aa852c387c90ed32ea28e0dd50d1c87ca218
xiao <603426335@qq.com>
2019-11-12 20:24:49 +0800

    生命游戏

---
+++ leecode/src/no0289/Test0289.java
@@ -0,0 +1,19 @@
+package no0289;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0289 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[][] board = {
+		                  {0,1,0},
+		                  {0,0,1},
+		                  {1,1,1},
+		                  {0,0,0}
+		                };
+		
+		so.gameOfLife(board);
+	}
+}
