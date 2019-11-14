734e4d09cb6f237bf5cdabb22a5b6d6d18abaa06
xiao <603426335@qq.com>
2019-11-14 16:12:19 +0800

    煎饼排序

---
+++ leecode/src/no0969/Test0969.java
@@ -0,0 +1,14 @@
+package no0969;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0969 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[] A = {1,2,3};
+		
+		so.pancakeSort(A);
+	}
+}
