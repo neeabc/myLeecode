fa6e8bd8a5c301bf8fcba2c2987238ddf0901230
xiao <603426335@qq.com>
2019-11-18 14:39:19 +0800

    划分字母区间

---
+++ leecode/src/no0763/Test0763.java
@@ -0,0 +1,14 @@
+package no0763;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0763 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		String S = "abc";
+		
+		so.partitionLabels(S);
+	}
+}
