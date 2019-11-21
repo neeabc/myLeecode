5dc0fae4886d238a6ce64e61b6a9ee9d28ad96a2
xiao <603426335@qq.com>
2019-11-21 20:07:26 +0800

    下降路径最小和

---
+++ leecode/src/no0931/Test0931.java
@@ -0,0 +1,14 @@
+package no0931;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0931 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int[][] A = {{1,2,3},{4,5,6},{7,8,9}};
+		
+		so.minFallingPathSum(A);
+	}
+}
