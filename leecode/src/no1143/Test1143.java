db0d4d17ce84f05e235be18cce4d82f8e0f6985d
xiao <603426335@qq.com>
2019-11-23 12:45:51 +0800

    最长公共子序列

---
+++ leecode/src/no1143/Test1143.java
@@ -0,0 +1,16 @@
+package no1143;
+
+import org.junit.jupiter.api.Test;
+
+public class Test1143 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		String text1 = "bcdea";
+		String text2 = "cbdf";
+		
+		
+		so.longestCommonSubsequence(text1, text2);
+	}
+}
