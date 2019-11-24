96c3e9c394ed78db249bdd2ac291c42e286bcb68
xiao <603426335@qq.com>
2019-11-24 13:16:00 +0800

    强整数

---
+++ leecode/src/no0970/Test0970.java
@@ -0,0 +1,16 @@
+package no0970;
+
+import org.junit.jupiter.api.Test;
+
+public class Test0970 {
+	@Test
+	public void startTest() {
+		Solution so = new Solution();
+		
+		int x = 2;
+		int y = 1;
+		int bound = 10;
+		
+		so.powerfulIntegers(x, y, bound);
+	}
+}
