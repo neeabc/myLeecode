08934720672bc6870c6f3fba49f85b160e7eac17
xiao <603426335@qq.com>
2020-06-12 17:13:36 +0800

    560. 和为K的子数组

---
+++ leecode/src/main/java/no05/no0560/Test0560.java
@@ -0,0 +1,11 @@
+package no05.no0560;
+
+import org.junit.Test;
+
+public class Test0560 {
+    @Test
+    public void test01(){
+        Solution so = new Solution();
+        so.subarraySum(new int[]{1,1,1}, 2);
+    }
+}
